5f4a160078b8574bee9311a23cc022a314064136
zaheedx13 <06zaheed@gmail.com>
2025-11-20 21:15:40 +0530

    1st commit

---
+++ src/main/java/Prac/asdfghjk/App.java
@@ -0,0 +1,11 @@
+package Prac.asdfghjk;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Yooo");
+    }
+}
